5e192d5b8d511f17750b02e12f154b59bbdb56b5
imjae <code@kakao.com>
2021-04-16 21:07:14 +0900

    HashRouter 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <h1>Home</h1>
+          </Route>
+          <Route path="/potato">
+            <h1>potato</h1>
+          </Route>
+          <Route path="/banana">
+            <h1>banana</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
