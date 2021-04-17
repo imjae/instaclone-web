b8c78c3de128491cad8bcd9933df13ec58cb0321
imjae <code@kakao.com>
2021-04-17 10:37:00 +0900

    Route구조 다시 완성 Home,Login,NotFound추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./screen/Home";
+import Login from "./screen/Login";
+import NotFound from "./screen/NotFound";
 
 function App() {
-  const isLoggedIn = false;
+  const isLoggedIn = true;
   return (
     <div>
       <Router>
         <Switch>
           <Route path="/" exact>
-            {isLoggedIn ? "Home" : "Login"}
+            {isLoggedIn ? <Home /> : <Login />}
+          </Route>
+          <Route>
+            <NotFound />
           </Route>
         </Switch>
       </Router>
