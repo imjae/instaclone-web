f1c42e178aa0c5597e8bb23b91d0d9557ca5e716
imjae <code@kakao.com>
2021-04-29 11:24:28 +0900

    client 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useReactiveVar } from "@apollo/client";
+import { ApolloProvider, useReactiveVar } from "@apollo/client";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import { isLoggedInVar, darkModeVar } from "./apollo";
+import { isLoggedInVar, darkModeVar, client } from "./apollo";
 import { ThemeProvider } from "styled-components";
 
 import Home from "./screen/Home";
@@ -15,24 +15,26 @@ function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
   return (
-    <HelmetProvider>
-      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-        <GlobalStyles />
-        <Router>
-          <Switch>
-            <Route path="/" exact>
-              {isLoggedIn ? <Home /> : <Login />}
-            </Route>
-            <Route path={routes.signUp} exact>
-              <SignUp />
-            </Route>
-            <Route>
-              <NotFound />
-            </Route>
-          </Switch>
-        </Router>
-      </ThemeProvider>
-    </HelmetProvider>
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+          <GlobalStyles />
+          <Router>
+            <Switch>
+              <Route path="/" exact>
+                {isLoggedIn ? <Home /> : <Login />}
+              </Route>
+              <Route path={routes.signUp} exact>
+                <SignUp />
+              </Route>
+              <Route>
+                <NotFound />
+              </Route>
+            </Switch>
+          </Router>
+        </ThemeProvider>
+      </HelmetProvider>
+    </ApolloProvider>
   );
 }
 
