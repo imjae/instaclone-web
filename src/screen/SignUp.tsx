3bce8c52decd5fd6e18408ab70519902776b457d
imjae <code@kakao.com>
2021-05-03 18:08:23 +0900

    버튼 error 로직 추가 전

--- src/screen/SignUp.tsx
+++ src/screen/SignUp.tsx
@@ -1,6 +1,4 @@
-import {
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 import AuthLayout from "../components/auth/AuthLayout";
@@ -12,6 +10,10 @@ import { FatLink } from "../components/shared";
 import Separator from "../components/auth/Separator";
 import routes from "../routes";
 import PageTitle from "../components/PageTitle";
+import { useForm } from "react-hook-form";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/client";
+import FormError from "../components/auth/FormError";
 
 const HeaderContainer = styled.div`
   display: flex;
@@ -32,27 +34,114 @@ const SubtitleSmall = styled.div`
   color: rgb(142, 142, 142);
 `;
 
+const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount(
+    $firstName: String!
+    $lastName: String
+    $userName: String!
+    $email: String!
+    $password: String!
+  ) {
+    createAccount(
+      firstName: $firstName
+      lastName: $lastName
+      userName: $userName
+      email: $email
+      password: $password
+    ) {
+      ok
+      error
+    }
+  }
+`;
+
 const SignUp = () => {
+  const [createAccount, { loading }] = useMutation(CREATE_ACCOUNT_MUTATION);
+  const { register, handleSubmit, errors, formState } = useForm({
+    mode: "onChange",
+  });
+  const onSubmitValid = (data: any) => {
+    if (loading) {
+      return;
+    }
+    createAccount({
+      variables: {
+        ...data,
+      },
+    });
+  };
+
   return (
     <AuthLayout>
-      <PageTitle title="Sign up"/>
+      <PageTitle title="Sign up" />
       <FormBox>
         <HeaderContainer>
           <FontAwesomeIcon icon={faInstagram} size="3x" />
-          <Subtitle>
-            친구들의 사진과 동영상을 보려면 가입하세요.
-          </Subtitle>
+          <Subtitle>친구들의 사진과 동영상을 보려면 가입하세요.</Subtitle>
           <Separator />
         </HeaderContainer>
-        <form>
-          <Input type="text" placeholder="휴대폰 번호 또는 이메일 주소" />
-          <Input type="text" placeholder="성명" />
-          <Input type="text" placeholder="사용자 이름" />
-          <Input type="password" placeholder="비밀번호" />
-          <Button type="submit" value="가입" />
+        <form onSubmit={handleSubmit(onSubmitValid)}>
+          <Input
+            ref={register({
+              required: "성을 입력하세요.",
+            })}
+            name="firstName"
+            type="text"
+            placeholder="성"
+            hasError={Boolean(errors?.firstName?.message)}
+          />
+          <FormError message={errors?.firstName?.message} />
+          <Input
+            ref={register}
+            name="lastName"
+            type="text"
+            placeholder="이름"
+          />
+          <Input
+            ref={register({
+              required: "이메일 주소를 입력하세요.",
+            })}
+            name="email"
+            type="text"
+            placeholder="이메일 주소"
+            hasError={Boolean(errors?.email?.message)}
+          />
+          <FormError message={errors?.email?.message} />
+          <Input
+            ref={register({
+              required: "사용자 이름을 입력해 주세요.",
+              minLength: {
+                value: 5,
+                message: "사용자 이름을 5글자 이상으로 입력해 주세요.",
+              },
+              // validate: (currentValue: any) => currentValue.includes("potato")
+            })}
+            name="userName"
+            type="text"
+            placeholder="사용자 이름"
+            hasError={Boolean(errors?.userName?.message)}
+          />
+          <FormError message={errors?.userName?.message} />
+          <Input
+            ref={register({
+              required: "비밀번호를 입력하세요.",
+            })}
+            name="password"
+            type="password"
+            placeholder="비밀번호"
+            hasError={Boolean(errors?.password?.message)}
+          />
+          <FormError message={errors?.password?.message} />
+          <Button
+            type="submit"
+            value={loading ? "Loading..." : "Sign up"}
+            disabled={!formState.isValid || loading}
+          />
         </form>
         <SubtitleSmall>
-          가입하면 Instagram의 <strong>약관</strong>, <strong>데이터 정책</strong> 및 <strong>쿠키 정책</strong>에 동의하게 됩니다.
+          가입하면 Instagram의 <strong>약관</strong>,{" "}
+          <strong>데이터 정책</strong> 및 <strong>쿠키 정책</strong>에 동의하게
+          됩니다.
         </SubtitleSmall>
       </FormBox>
       <BottomBox
