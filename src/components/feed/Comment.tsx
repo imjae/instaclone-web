b0add10b6502fb7698e51eeac45f62249487b4f3
imjae <code@kakao.com>
2021-07-18 16:58:54 +0900

    hashtag 링크처리

--- src/components/feed/Comment.tsx
+++ src/components/feed/Comment.tsx
@@ -1,17 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import { FatText } from "../shared";
 import styled from "styled-components";
 
-const CommentsContainer = styled.div`
-`;
+const CommentsContainer = styled.div``;
 const CommentCaption = styled.span`
-margin-left: 10px;
+  margin-left: 10px;
+  a {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 
-const Comment = ({author, payload}: any) => {
+const Comment = ({ author, payload }: any) => {
   return (
     <CommentsContainer>
       <FatText>{author}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption>
+        {payload.split(" ").map((word: string, index: number) =>
+          /#[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+/.test(word) ? (
+            <React.Fragment key={index}>
+              <Link to={`/hashtags/${word}`}>{word}</Link>{" "}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>{word} </React.Fragment>
+          )
+        )}
+      </CommentCaption>
     </CommentsContainer>
   );
 };
